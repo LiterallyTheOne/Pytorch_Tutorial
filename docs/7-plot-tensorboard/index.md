dbe3319752cdb7eadec4346a6a43a5934470ecdd
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-08-27 13:06:38 +0330

    Add tensorboard

--- docs/7-plot-tensorboard/index.md
+++ docs/7-plot-tensorboard/index.md
@@ -88,6 +88,83 @@ So, the output would be something like below:
 
 As you can see, analyzing the plots is so much easier than examining the numbers.
 
+## TensorBoard
+
+`TensorBoard` is one of the most used and greatest tools to keep track of our training.
+It has so many features, but we are going to focus on only plotting.
+To do so, we should have a `TensorBoard writer`.
+So, let's make one.
+
+```python
+from torch.utils.tensorboard import SummaryWriter
+
+# -------------------[ Setup TensorBoard ]-------------------
+writer = SummaryWriter()
+```
+
+In the code above, I have imported the `SummaryWriter` from `torch.utils.tensorboard`.
+Then, I have created an instance of `SummaryWriter` and called it `writer`.
+By default, `SummaryWriter` creates a log directory with the name of `runs` and stores the data into that directory.
+Now, let's write our training and evaluation results with `writer`.
+
+```python
+# -------------------[ Train and evaluate the model ]-------------------
+for epoch in range(20):
+    print("-" * 20)
+    print(f"epoch: {epoch}")
+
+    train_loss, train_accuracy = train_step(train_loader, model, optimizer, loss_fn, device)
+    writer.add_scalar("loss/train", train_loss, epoch)
+    writer.add_scalar("accuracy/train", train_accuracy, epoch)
+
+    val_loss, val_accuracy = val_step(val_loader, model, loss_fn, device)
+    writer.add_scalar("loss/val", val_loss, epoch)
+    writer.add_scalar("accuracy/val", val_accuracy, epoch)
+
+    print(f"train: ")
+    print(f"\tloss: {train_loss:.4f}")
+    print(f"\taccuracy: {train_accuracy:.4f}")
+
+    print(f"validation: ")
+    print(f"\tloss: {val_loss:.4f}")
+    print(f"\taccuracy: {val_accuracy:.4f}")
+```
+
+As you can see, in our `training loop`, I used the `add_scalar` function of the `writer` to write the results.
+For each result, I have chosen different names.
+
+* `train_loss` -> `loss/train`
+* `train_accuary` -> `loss/accuracy`
+* `val_loss` -> `loss/val`
+* `val_accuary` -> `loss/accuracy`
+
+I have applied the changes to
+[train_tensorboard.py](https://github.com/LiterallyTheOne/Pytorch_Tutorial/blob/main/src/7_plot_tensorboard/train_tensorboard.py).
+Now, let's write our training script multiple times.
+For example, I have run it `3` times.
+After that, let's run our `TensorBoard` to see the results.
+To do so, we can run the command below:
+
+```shell
+tensorboard --log_dir runs
+```
+
+Or if you want to see the results on a notebook, you can use the code below:
+
+```text
+%load_ext tensorboard
+%tensorboard --logdir runs
+```
+
+The output would be something like below:
+
+![tensorboard result](tensorboard.webp)
+
+As you can see, my `3` runs are being displayed separately.
+Right now, I can easily analyze my training.
+Also, because I write the results into files, I can see the results during the training process,
+which is extremely helpful
+
 
 
 
---
+++ src/7_plot_tensorboard/train_tensorboard.py
@@ -0,0 +1,192 @@
+# -------------------[ Imports ]-------------------
+import torch
+from torch import nn
+from torch.optim import Adam, Optimizer
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.tensorboard import SummaryWriter
+
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+
+# -------------------[ Define Dataset ]-------------------
+class IRISDataset(Dataset):
+    def __init__(self, data, target):
+        super().__init__()
+        self.data = data
+        self.target = target
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data = torch.tensor(self.data[idx]).to(torch.float)
+        target = torch.tensor(self.target[idx])
+        return data, target
+
+
+# -------------------[ Define Model ]-------------------
+class IRISClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(4, 16),
+            nn.Linear(16, 8),
+            nn.Linear(8, 3),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+# -------------------[ Define Training step ]-------------------
+def train_step(
+        data_loader: DataLoader,
+        model: nn.Module,
+        optimizer: Optimizer,
+        loss_fn: nn.Module,
+        device: str,
+) -> tuple[float, float]:
+    model.train()
+
+    total_loss = 0
+    total_correct = 0
+
+    for batch_of_data, batch_of_target in data_loader:
+        batch_of_data = batch_of_data.to(device)
+        batch_of_target = batch_of_target.to(device)
+
+        optimizer.zero_grad()
+
+        logits = model(batch_of_data)
+
+        loss = loss_fn(logits, batch_of_target)
+        total_loss += loss.item()
+
+        predictions = logits.argmax(dim=1)
+        total_correct += predictions.eq(batch_of_target).sum().item()
+
+        loss.backward()
+
+        optimizer.step()
+
+    return total_loss / len(data_loader), total_correct / len(data_loader.dataset)
+
+
+# -------------------[ Define Validation Step ]-------------------
+def val_step(
+        data_loader: DataLoader,
+        model: nn.Module,
+        loss_fn: nn.Module,
+        device: str,
+) -> tuple[float, float]:
+    model.eval()
+
+    with torch.inference_mode():
+        total_loss = 0
+        total_correct = 0
+
+        for batch_of_data, batch_of_target in data_loader:
+            batch_of_data = batch_of_data.to(device)
+            batch_of_target = batch_of_target.to(device)
+
+            logits = model(batch_of_data)
+
+            loss = loss_fn(logits, batch_of_target)
+            total_loss += loss.item()
+
+            predictions = logits.argmax(dim=1)
+            total_correct += predictions.eq(batch_of_target).sum().item()
+
+        return total_loss / len(data_loader), total_correct / len(data_loader.dataset)
+
+
+def main():
+    # -------------------[ Find the accelerator ]-------------------
+    if torch.accelerator.is_available():
+        device = torch.accelerator.current_accelerator()
+    else:
+        device = "cpu"
+
+    print(device)
+
+    # -------------------[ Load the data ]-------------------
+    iris = load_iris()
+
+    # -------------------[ Split the data to train, validation, and test ]-------------------
+    train_subset, val_subset, train_target, val_target = train_test_split(
+        iris.data,
+        iris.target,
+        test_size=0.3,
+        random_state=42,
+        stratify=iris.target,
+    )
+    val_subset, test_subset, val_target, test_target = train_test_split(
+        val_subset,
+        val_target,
+        test_size=0.33,
+        random_state=42,
+        stratify=val_target,
+    )
+
+    # -------------------[ Normalize subsets ]-------------------
+    scaler = StandardScaler()
+    scaler.fit(train_subset)
+
+    train_subset_normalized = scaler.transform(train_subset)
+    val_subset_normalized = scaler.transform(val_subset)
+    test_subset_normalized = scaler.transform(test_subset)
+
+    train_data = IRISDataset(train_subset_normalized, train_target)
+    val_data = IRISDataset(val_subset_normalized, val_target)
+    test_data = IRISDataset(test_subset_normalized, test_target)
+
+    # -------------------[ Create data Loaders ]-------------------
+    train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=10, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=10, shuffle=False)
+
+    # -------------------[ Create the model ]-------------------
+    model = IRISClassifier()
+    model.to(device)
+
+    # -------------------[ Define loss function and optimizer ]-------------------
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters())
+
+    # -------------------[ Setup TensorBoard ]-------------------
+    writer = SummaryWriter()
+
+    # -------------------[ Train and evaluate the model ]-------------------
+    for epoch in range(20):
+        print("-" * 20)
+        print(f"epoch: {epoch}")
+
+        train_loss, train_accuracy = train_step(train_loader, model, optimizer, loss_fn, device)
+        writer.add_scalar("loss/train", train_loss, epoch)
+        writer.add_scalar("accuracy/train", train_accuracy, epoch)
+
+        val_loss, val_accuracy = val_step(val_loader, model, loss_fn, device)
+        writer.add_scalar("loss/val", val_loss, epoch)
+        writer.add_scalar("accuracy/val", val_accuracy, epoch)
+
+        print(f"train: ")
+        print(f"\tloss: {train_loss:.4f}")
+        print(f"\taccuracy: {train_accuracy:.4f}")
+
+        print(f"validation: ")
+        print(f"\tloss: {val_loss:.4f}")
+        print(f"\taccuracy: {val_accuracy:.4f}")
+
+    print("-" * 20)
+    test_loss, test_accuracy = val_step(test_loader, model, loss_fn, device)
+    print(f"test: ")
+    print(f"\tloss: {test_loss:.4f}")
+    print(f"\taccuracy: {test_accuracy:.4f}")
+
+
+if __name__ == "__main__":
+    main()
